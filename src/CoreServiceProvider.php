32a5dfd5f01f0765a50706c8f97d00acd1a1b831
aocneanu <aocneanu@gmail.com>
2017-04-09 17:30:16 +0000

    Apply fixes from StyleCI

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -60,104 +60,104 @@ class CoreServiceProvider extends ServiceProvider
     private function publishesDepedencies()
     {
         $this->publishes([
-            __DIR__ . '/config/laravel-enso.php' => config_path('laravel-enso.php'),
+            __DIR__.'/config/laravel-enso.php' => config_path('laravel-enso.php'),
         ], 'core-config');
 
         $this->publishes([
-            __DIR__ . '/database/migrations' => database_path('migrations'),
+            __DIR__.'/database/migrations' => database_path('migrations'),
         ], 'core-migrations');
 
         $this->publishes([
-            __DIR__ . '/resources/routes/web.php' => base_path('routes/web.php'),
+            __DIR__.'/resources/routes/web.php' => base_path('routes/web.php'),
         ], 'core-routes');
 
         $this->publishes([
-            __DIR__ . '/resources/lang' => resource_path('lang'),
+            __DIR__.'/resources/lang' => resource_path('lang'),
         ], 'core-lang');
     }
 
     private function publishesClasses()
     {
         $this->publishes([
-            __DIR__ . '/resources/Classes/DataTable' => app_path('DataTable'),
+            __DIR__.'/resources/Classes/DataTable' => app_path('DataTable'),
         ], 'core-classes');
 
         $this->publishes([
-            __DIR__ . '/resources/Classes/Controllers' => app_path('Http/Controllers'),
+            __DIR__.'/resources/Classes/Controllers' => app_path('Http/Controllers'),
         ], 'core-controllers');
 
         $this->publishes([
-            __DIR__ . '/resources/Classes/Requests' => app_path('Http/Requests'),
+            __DIR__.'/resources/Classes/Requests' => app_path('Http/Requests'),
         ], 'core-requests');
 
         $this->publishes([
-            __DIR__ . '/resources/Classes/Models' => app_path(),
+            __DIR__.'/resources/Classes/Models' => app_path(),
         ], 'core-models');
 
         $this->publishes([
-            __DIR__ . '/app/Notifications' => app_path('Notifications/vendor/laravel-enso'),
+            __DIR__.'/app/Notifications' => app_path('Notifications/vendor/laravel-enso'),
         ], 'core-notification');
     }
 
     private function publishesViews()
     {
         $this->publishes([
-            __DIR__ . '/resources/views/layouts' => resource_path('views/vendor/laravel-enso/core/layouts'),
+            __DIR__.'/resources/views/layouts' => resource_path('views/vendor/laravel-enso/core/layouts'),
         ], 'core-layouts-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/partials' => resource_path('views/vendor/laravel-enso/core/partials'),
+            __DIR__.'/resources/views/partials' => resource_path('views/vendor/laravel-enso/core/partials'),
         ], 'core-partials-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/includes' => resource_path('views/vendor/laravel-enso/core/includes'),
+            __DIR__.'/resources/views/includes' => resource_path('views/vendor/laravel-enso/core/includes'),
         ], 'core-includes-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/pages' => resource_path('views/vendor/laravel-enso/core/pages'),
+            __DIR__.'/resources/views/pages' => resource_path('views/vendor/laravel-enso/core/pages'),
         ], 'core-pages-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/pages/administration' => resource_path('views/vendor/laravel-enso/core/pages/administration'),
+            __DIR__.'/resources/views/pages/administration' => resource_path('views/vendor/laravel-enso/core/pages/administration'),
         ], 'core-administration-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/auth' => resource_path('views/auth'),
+            __DIR__.'/resources/views/auth' => resource_path('views/auth'),
         ], 'core-auth-views');
 
         $this->publishes([
-            __DIR__ . '/resources/views/errors' => resource_path('views/errors'),
+            __DIR__.'/resources/views/errors' => resource_path('views/errors'),
         ], 'core-errors');
     }
 
     private function publishesResources()
     {
         $this->publishes([
-            __DIR__ . '/resources/storage' => storage_path('app'),
+            __DIR__.'/resources/storage' => storage_path('app'),
         ], 'core-storage');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/images' => resource_path('assets/images'),
+            __DIR__.'/resources/assets/images' => resource_path('assets/images'),
         ], 'core-images');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/js' => resource_path('assets/js/vendor/laravel-enso'),
+            __DIR__.'/resources/assets/js' => resource_path('assets/js/vendor/laravel-enso'),
         ], 'core-js');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/libs' => resource_path('assets/libs'),
+            __DIR__.'/resources/assets/libs' => resource_path('assets/libs'),
         ], 'core-libs');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/main-js' => resource_path('assets/js'),
+            __DIR__.'/resources/assets/main-js' => resource_path('assets/js'),
         ], 'core-main-js');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/sass' => resource_path('assets/sass'),
+            __DIR__.'/resources/assets/sass' => resource_path('assets/sass'),
         ], 'core-sass');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/root' => base_path(),
+            __DIR__.'/resources/assets/root' => base_path(),
         ], 'core-root');
     }
 
@@ -179,9 +179,9 @@ class CoreServiceProvider extends ServiceProvider
 
     private function loadDependencies()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'laravel-enso/core');
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/core');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     private function registerComposers()
