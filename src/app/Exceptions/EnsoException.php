28bc7b7afbd11138ff995704b66a115aa4e4b5b7
mishu <mishu@xtelecom.ro>
2017-04-10 13:59:13 +0300

    added level to EnsoException;

--- src/app/Exceptions/EnsoException.php
+++ src/app/Exceptions/EnsoException.php
@@ -6,5 +6,16 @@ use Exception;
 
 class EnsoException extends Exception
 {
-    //
+    private $level;
+
+    public function __construct(string $message, string $level = 'error', int $code = 400)
+    {
+    	$this->level = $level;
+    	parent::__construct(__($message), $code);
+    }
+
+    public function getLevel()
+    {
+    	return $this->level;
+    }
 }
