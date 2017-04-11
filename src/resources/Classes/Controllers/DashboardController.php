0892f81760a520455727d2b889602e97f8a45d61
aocneanu <aocneanu@gmail.com>
2017-04-11 14:50:48 +0000

    Apply fixes from StyleCI

--- src/resources/Classes/Controllers/DashboardController.php
+++ src/resources/Classes/Controllers/DashboardController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use LaravelEnso\Charts\app\Classes\BarChart;
 use LaravelEnso\Charts\app\Classes\BubbleChart;
 use LaravelEnso\Charts\app\Classes\LineChart;
