f525ce9395a5f331e0bc966ac66452549cbe5e06
mishu <mishu@xtelecom.ro>
2017-04-22 17:23:48 +0300

    removed StatisticsController

--- src/resources/assets/js/components/Dashboard.vue
+++ src/resources/assets/js/components/Dashboard.vue
@@ -1,58 +1,56 @@
 <template>
-	<div class="row">
-		<draggable class="col-md-4"
-			v-for="(charts, column) in configuration.charts"
-			:list="charts"
-			:ref="'column_' + column"
-			:options="{ handle: '.draggable', group: 'dashboard' }"
-			@update="updateContent(column)"
-			@add="updateContent(column)"
-			@remove="updateContent(column)"
-			v-if="isVisible">
-			<div v-for="(chart, index) in charts"
-				:key="index">
-				<chart :type="chart.type"
-					:source="chart.source"
+    <div class="row">
+        <draggable class="col-md-6"
+            v-for="(charts, column) in configuration.charts"
+            :list="charts"
+            :ref="'column_' + column"
+            :options="{ handle: '.draggable', group: 'dashboard' }"
+            @update="updateContent(column)"
+            @add="updateContent(column)"
+            @remove="updateContent(column)"
+            v-if="isVisible">
+            <div v-for="(chart, index) in charts"
+                :key="index">
+                <chart :type="chart.type"
+                    :source="chart.source"
+                    :params="params"
                     draggable
-					:collapsed="chart.collapsed"
-					@changed-state="updateState($event, chart.source)">
-					<span slot="chart-title">{{ chart.title }}</span>
-				</chart>
-			</div>
-		</draggable>
-	</div>
+                    :collapsed="chart.collapsed"
+                    @changed-state="updateState($event, chart.source)">
+                    <span slot="chart-title">{{ chart.title }}</span>
+                </chart>
+            </div>
+        </draggable>
+    </div>
 </template>
 
 <script>
 
-	export default {
-
-		props: {
-
-			preferences: String,
-			required: true
-		},
-		watch: {
-
+    export default {
+        props: {
+            preferences: {
+                type: String,
+                required: true
+            },
+            params: {
+                type: Object
+            }
+        },
+        watch: {
             configuration: {
-
                 handler: 'updatePreferences',
                 deep: true
             }
         },
         data: function() {
-
-        	return {
-
-	            configuration: JSON.parse(this.preferences),
-	            isVisible: true
-	        }
+            return {
+                configuration: JSON.parse(this.preferences),
+                isVisible: true
+            }
         },
         methods: {
             updateContent: function(column) {
-
                 this.$nextTick(function() {
-
                         this.$refs['column_' + column][0].$children.forEach(function(child) {
 
                         child.getData();
@@ -60,23 +58,18 @@
                 });
             },
             updatePreferences: function() {
-
                 if (!this.settingPreferences) {
-
                     this.settingPreferences = true
 
                     axios.patch('/core/preferences/setPreferences', {key: 'dashboard', value: JSON.stringify(this.configuration)}).then(() => {
-
                         this.settingPreferences = false;
                     });
                 }
             },
             resetToDefault: function() {
-
                 this.isVisible = false;
 
                 axios.post('/core/preferences/resetToDefaut', { key: 'dashboard' }).then((response) => {
-
                     this.configuration = response.data;
                     this.isVisible = true;
                 });
