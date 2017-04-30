aed18253a9e9e32a0ef93d4e699dbef481531340
mishu <mishu@xtelecom.ro>
2017-04-30 10:51:35 +0300

    added $errorBag array to EnsoException

--- src/app/Exceptions/EnsoException.php
+++ src/app/Exceptions/EnsoException.php
@@ -8,14 +8,20 @@ class EnsoException extends Exception
 {
     private $level;
 
-    public function __construct(string $message, string $level = 'error', int $code = 400)
+    public function __construct(string $message, string $level = 'error', array $errorBag = [], int $code = 400)
     {
         $this->level = $level;
+        $this->errorBag = $errorBag;
         parent::__construct(__($message), $code);
     }
 
     public function getLevel()
     {
         return $this->level;
+    }
+
+    public function getErrorBag()
+    {
+        return $this->errorBag;
     }
 }
