92c0b3424f456610ca9e189508217bb1d8412053
mishu <mishu@xtelecom.ro>
2017-06-23 15:35:49 +0300

    imported the user in generateUserExportJob

--- src/app/Jobs/GenerateUsersExportJob.php
+++ src/app/Jobs/GenerateUsersExportJob.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use LaravelEnso\Core\app\Enums\IsActiveEnum;
+use LaravelEnso\Core\app\Models\User;
 use LaravelEnso\Core\app\Notifications\UsersExportNotification;
 
 class GenerateUsersExportJob implements ShouldQueue
