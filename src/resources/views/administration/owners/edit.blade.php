a99d11895ad60c3631a4bd6960be6a403e336f9d
mishu <mishu@xtelecom.ro>
2017-06-27 22:07:02 +0300

    removed unused view. conditionally added commments and documents components for testing purposes

--- src/resources/views/administration/owners/edit.blade.php
+++ src/resources/views/administration/owners/edit.blade.php
@@ -48,20 +48,24 @@
                     </div>
                 </div>
 
-                <!-- <comments-manager :id="{{ $owner->id }}"
-                    type="owner"
-                    v-if="{{ $owner }}"
-                    edited-label="{{ __("edited") }}">
-                    <span slot="comments-manager-title">{{ __("Comments") }}</span>
-                    <span slot="comments-manager-load-more">{{ __("more") }}</span>
-                </comments-manager>
-                <documents-manager :id="{{ $owner->id }}"
-                    :file-size-limit="5000000"
-                    type="owner"
-                    v-if="{{ $owner }}">
-                    <span slot="documents-manager-title">{{ __("Documents") }}</span>
-                    @include('laravel-enso/core::partials.modal')
-                </documents-manager> -->
+                @if(!empty(config('comments.commentables')))
+                    <comments-manager :id="{{ $owner->id }}"
+                        type="owner"
+                        v-if="{{ $owner }}"
+                        edited-label="{{ __("edited") }}">
+                        <span slot="comments-manager-title">{{ __("Comments") }}</span>
+                        <span slot="comments-manager-load-more">{{ __("more") }}</span>
+                    </comments-manager>
+                @endif
+                @if(!empty(config('documents.documentables')))
+                    <documents-manager :id="{{ $owner->id }}"
+                        :file-size-limit="5000000"
+                        type="owner"
+                        v-if="{{ $owner }}">
+                        <span slot="documents-manager-title">{{ __("Documents") }}</span>
+                        @include('laravel-enso/core::partials.modal')
+                    </documents-manager>
+                @endif
 
             </div>
         </div>
