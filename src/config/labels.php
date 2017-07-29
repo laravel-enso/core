915974d97b9d357c8dd71d2eff11dd1ab8e3fa0c
Adrian Ocneanu <aocneanu@gmail.com>
2017-07-29 11:41:42 +0000

    Apply fixes from StyleCI

--- src/config/labels.php
+++ src/config/labels.php
@@ -18,5 +18,5 @@ return [
     'comments'            => 'Comments',
     'more'                => 'more',
     'documents'           => 'Documents',
-    'totals'              => 'Totals'
+    'totals'              => 'Totals',
 ];
