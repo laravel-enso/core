2a3558e83c19362ad8ebcb4e5e3411d8459600aa
mishu <mishu@xtelecom.ro>
2017-08-05 09:59:44 +0300

    fix auth scss. added particles.scss for background customisation

--- src/resources/views/layouts/auth.blade.php
+++ src/resources/views/layouts/auth.blade.php
@@ -11,9 +11,9 @@
         @include('laravel-enso/core::includes.authCss')
 
         <link rel = "icon" href = "/images/favicon.ico"/>
-        <link rel="stylesheet" type="text/css" href="/css/app.css"/>
         <link rel="stylesheet" type="text/css" href="/css/enso.css"/>
         <link rel="stylesheet" type="text/css" href="/css/auth.css"/>
+        <link rel="stylesheet" type="text/css" href="/css/particles.css"/>
     </head>
 
     <body class="hold-transition login-page">
