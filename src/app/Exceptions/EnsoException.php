2569bb70304fe0cf59f866c494589ff045332a04
mishu <mishu@xtelecom.ro>
2017-09-26 14:25:00 +0300

    removed unused var

--- src/app/Exceptions/EnsoException.php
+++ src/app/Exceptions/EnsoException.php
@@ -6,8 +6,6 @@ use Exception;
 
 class EnsoException extends Exception
 {
-    private $level;
-
     public function __construct(string $message, int $code = 400)
     {
         parent::__construct(__($message), $code);
