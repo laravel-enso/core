933c62b1969182445ca25289f05f00daa6d1a672
Mihai <mihai.ocneanu@gmail.com>
2017-09-29 13:28:22 +0300

    added sass folder that got lost in the conversion | cleaned up sass folder to exclude app.js; removed pusher key from blade

--- src/resources/views/home/index.blade.php
+++ src/resources/views/home/index.blade.php
@@ -3,7 +3,6 @@
         <meta charset="utf-8"/>
         <meta content="width=device-width, initial-scale=1" name="viewport"/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="pusher-key" content="">
 
         <title>{{ config('app.name') }}</title>
 
