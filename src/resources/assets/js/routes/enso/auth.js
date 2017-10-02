adfa75ed1016da4022e6516e9c2669a8509a9e25
Mihai <mihai.ocneanu@gmail.com>
2017-10-02 14:38:22 +0300

    fixed lower case import

--- src/resources/assets/js/routes/enso/auth.js
+++ src/resources/assets/js/routes/enso/auth.js
@@ -1,5 +1,5 @@
 export const Auth = [
 	{ path: '/login', name: 'login', component: require('../../pages/enso/auth/Login.vue') },
-    { path: '/password/reset', name: 'password.email', component: require('../../pages/enso/auth/password/email.vue') }
+    { path: '/password/reset', name: 'password.email', component: require('../../pages/enso/auth/password/Email.vue') }
     // { path: '/password/reset/:token', name: 'password.reset', component: require('../pages/enso/auth/password/reset.vue') }
 ];
