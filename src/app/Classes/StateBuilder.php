5764fec60747059ce47be6f75195687fdccf9516
Adrian Ocneanu <aocneanu@gmail.com>
2017-10-05 17:58:35 +0000

    Apply fixes from StyleCI

--- src/app/Classes/StateBuilder.php
+++ src/app/Classes/StateBuilder.php
@@ -26,7 +26,7 @@ class StateBuilder
     private function setState()
     {
         $languages = Language::get(['name', 'flag']);
-        $menus     = $this->getMenus();
+        $menus = $this->getMenus();
 
         $this->state = [
             'user'          => $this->user,
@@ -58,7 +58,7 @@ class StateBuilder
             }
 
             $json = json_decode(\File::get(
-                resource_path('lang' . DIRECTORY_SEPARATOR . $lang->name . '.json')
+                resource_path('lang'.DIRECTORY_SEPARATOR.$lang->name.'.json')
             ));
 
             $i18n[$lang->name] = $json;
@@ -71,7 +71,7 @@ class StateBuilder
     {
         return [
             'appName'    => config('app.name'),
-            'appUrl'     => url('/') . '/',
+            'appUrl'     => url('/').'/',
             'version'    => config('enso.config.version'),
             'quote'      => Inspiring::quote(),
             'env'        => config('app.env'),
