187f3d6a63150465b88e60542ff1ffd5bb6888ad
mishu <mishu@xtelecom.ro>
2017-10-09 18:06:46 +0300

    added polyfills

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -48,6 +48,22 @@
 
         </div>
 
+        @php
+            $polyfills = collect([
+                'Promise',
+                'Object.assign',
+                'Object.values',
+                'Array.prototype.find',
+                'Array.prototype.findIndex',
+                'Array.prototype.includes',
+                'String.prototype.includes',
+                'String.prototype.startsWith',
+                'String.prototype.endsWith'
+            ])->implode(',');
+        @endphp
+
+        <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features={{ $polyfills }}"></script>
+
         <script>window.Store = {!! $store !!};</script>
 
         <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
