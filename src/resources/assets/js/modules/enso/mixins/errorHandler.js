66ae3d594b1a86dddd630b52c4475962bad906f3
mishu <mishu@xtelecom.ro>
2017-11-07 16:28:24 +0200

    fix #9

--- src/resources/assets/js/modules/enso/mixins/errorHandler.js
+++ src/resources/assets/js/modules/enso/mixins/errorHandler.js
@@ -3,7 +3,7 @@ Vue.mixin({
         handleError(error) {
             let { status, data } = error.response;
 
-            if (data.status === 401) {
+            if (status === 401) {
                 if (this.$store.getters['auth/isAuth']) {
                     this.$store.dispatch('auth/logout');
                 }
