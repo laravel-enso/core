244443bd47054384d748a60e84b40268d737b5f6
mishu <mishu@xtelecom.ro>
2018-01-11 10:50:19 +0200

    renames app service provider; relocates js assets

---
+++ src/AppServiceProvider.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace LaravelEnso\Core;
+
+use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Core\app\Http\Middleware\VerifyActiveState;
+use LaravelEnso\Impersonate\app\Http\Middleware\Impersonate;
+use LaravelEnso\Localisation\app\Http\Middleware\SetLanguage;
+use LaravelEnso\ActionLogger\app\Http\Middleware\ActionLogger;
+use LaravelEnso\PermissionManager\app\Http\Middleware\VerifyRouteAccess;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishesDependencies(); //fixme we should publish the notification for password reset!?
+        $this->publishesResources();
+        $this->registerMiddleware();
+        $this->loadDependencies();
+    }
+
+    private function publishesDependencies()
+    {
+        $this->publishes([
+            __DIR__.'/config' => config_path('enso'),
+        ], 'core-config');
+
+        $this->publishes([
+            __DIR__.'/config' => config_path('enso'),
+        ], 'enso-config');
+
+        $this->publishes([
+            __DIR__.'/resources/preferences.json' => resource_path('preferences.json'),
+        ], 'core-preferences');
+
+        $this->publishes([
+            __DIR__.'/resources/preferences.json' => resource_path('preferences.json'),
+        ], 'enso-preferences');
+
+        $this->publishes([
+            __DIR__.'/resources/lang' => resource_path('lang'),
+        ], 'core-lang');
+    }
+
+    private function publishesResources()
+    {
+        $this->publishes([
+            __DIR__.'/storage' => storage_path('app'),
+        ], 'core-storage');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+            __DIR__.'/resources/assets/sass' => resource_path('assets/sass'),
+        ], 'core-assets');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js'),
+            __DIR__.'/resources/assets/sass' => resource_path('assets/sass'),
+        ], 'enso-assets');
+
+        $this->publishes([
+            __DIR__.'/resources/views' => resource_path('views/vendor/laravel-enso'),
+        ]);
+    }
+
+    private function registerMiddleware()
+    {
+        $this->app['router']->aliasMiddleware('verify-active-state', VerifyActiveState::class);
+
+        $this->app['router']->middlewareGroup('core', [
+            VerifyActiveState::class,
+            ActionLogger::class,
+            VerifyRouteAccess::class,
+            Impersonate::class,
+            SetLanguage::class,
+        ]);
+    }
+
+    private function loadDependencies()
+    {
+        $this->mergeConfigFrom(__DIR__.'/config/inspiring.php', 'enso.inspiring');
+        $this->mergeConfigFrom(__DIR__.'/config/labels.php', 'enso.labels');
+        $this->mergeConfigFrom(__DIR__.'/config/config.php', 'enso.config');
+        $this->mergeConfigFrom(__DIR__.'/config/themes.php', 'enso.themes');
+        $this->loadRoutesFrom(__DIR__.'/routes/api.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'laravel-enso/core');
+    }
+
+    public function register()
+    {
+        //
+    }
+}
