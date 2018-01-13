cf5e52ddfc72f8e6dce08c025cea5fb8c956df19
mishu <mishu@xtelecom.ro>
2018-01-13 14:24:21 +0200

    adds addresses to owner

--- src/resources/assets/js/pages/administration/owners/Edit.vue
+++ src/resources/assets/js/pages/administration/owners/Edit.vue
@@ -15,6 +15,9 @@
             <documents :id="owner.id"
                 type="owner">
             </documents>
+            <addresses :id="owner.id"
+                type="owner">
+            </addresses>
         </div>
     </div>
 
@@ -25,11 +28,12 @@
 import Documents from '../../../components/enso/documents/Documents.vue';
 import Comments from '../../../components/enso/comments/Comments.vue';
 import Contacts from '../../../components/enso/contacts/Contacts.vue';
+import Addresses from '../../../components/enso/addresses/Addresses.vue';
 import VueForm from '../../../components/enso/vueforms/VueForm.vue';
 
 export default {
     components: {
-        Comments, Contacts, Documents, VueForm,
+        Comments, Contacts, Documents, Addresses, VueForm,
     },
 
     data() {
