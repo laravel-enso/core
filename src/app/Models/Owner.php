f4f27709c10dd37564c91c53b129616a5d0497ac
mishu <mishu@xtelecom.ro>
2018-01-18 21:41:01 +0200

    adds HasRole trait to Owner;

--- src/app/Models/Owner.php
+++ src/app/Models/Owner.php
@@ -4,12 +4,12 @@ namespace LaravelEnso\Core\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use LaravelEnso\Helpers\app\Traits\IsActive;
-use LaravelEnso\RoleManager\app\Models\Role;
+use LaravelEnso\RoleManager\app\Traits\HasRoles;
 use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class Owner extends Model
 {
-    use IsActive;
+    use IsActive, HasRoles;
 
     protected $fillable = ['name', 'description', 'is_active'];
 
@@ -20,34 +20,6 @@ class Owner extends Model
     public function users()
     {
         return $this->hasMany(User::class);
-    }
-
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class);
-    }
-
-    public function getRoleListAttribute()
-    {
-        return $this->roles->pluck('id');
-    }
-
-    public function updateWithRoles(array $attributes, array $roles)
-    {
-        tap($this)->update($attributes)
-            ->roles()
-            ->sync($roles);
-    }
-
-    public function storeWithRoles(array $attributes, array $roles)
-    {
-        $this->fill($attributes);
-
-        tap($this)->save()
-            ->roles()
-            ->sync($roles);
-
-        return $this;
     }
 
     public function delete()
