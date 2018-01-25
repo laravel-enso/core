a0ec48ca098f06c09d7c3f583e8111434bd814d0
mishu <mishu@xtelecom.ro>
2018-01-25 18:09:12 +0200

    fixes enso.js

--- src/resources/assets/js/enso.js
+++ src/resources/assets/js/enso.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import axios from 'axios';
-import lodash from 'lodash';
 import moment from 'moment';
+import fontawesome from '@fortawesome/fontawesome';
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import { faListAlt } from '@fortawesome/fontawesome-free-solid/shakable.es';
+import { faGithub } from '@fortawesome/fontawesome-free-brands/shakable.es';
 
 import './modules/enso/';
 
@@ -13,8 +15,9 @@ import Toastr from './components/enso/bulma/toastr';
 
 import './app';
 
-Vue.component('fa', FontAwesomeIcon);
+fontawesome.library.add(faListAlt, faGithub);
 
+Vue.component('fa', FontAwesomeIcon);
 Vue.use(Toastr, {
     i18n: store.getters['locale/__'],
     position: 'right',
@@ -25,7 +28,6 @@ Vue.use(Toastr, {
 const bus = new Vue({ name: 'Bus' });
 Vue.prototype.$bus = bus;
 
-window._ = lodash;
 window.moment = moment;
 window.axios = axios;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
