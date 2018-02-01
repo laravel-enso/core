227ff14545a46f55f10174f5ccd555874b2104b3
mishu <mishu@xtelecom.ro>
2018-02-01 15:05:54 +0200

    fixes #37

--- src/resources/assets/js/pages/layout/sidebar/icons/app.js
+++
@@ -1,9 +0,0 @@
-// import fontawesome from '@fortawesome/fontawesome';
-
-// import { } from '@fortawesome/fontawesome-free-regular/shakable.es'
-
-// import { } from '@fortawesome/fontawesome-free-solid/shakable.es';
-
-// import { } from '@fortawesome/fontawesome-free-brands/shakable.es';
-
-// fontawesome.library.add([]);
