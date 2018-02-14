5537e347064d0e93c0e20248aad368840c872b82
mishu <mishu@xtelecom.ro>
2018-02-14 15:29:06 +0200

    adds html5 doctype

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,7 +1,8 @@
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8"/>
-        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <link rel="icon" href="/favicon.ico"/>
         <link id="theme" rel="stylesheet" type="text/css" href="/themes/clean/bulma.min.css">
