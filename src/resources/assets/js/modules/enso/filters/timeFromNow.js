99e72fe0295f0f1d872c3e7f97eacbee79c5e9d0
mishu <mishu@xtelecom.ro>
2018-02-17 08:26:52 +0200

    refactors users show.vue; uses tree-shaking for date-fns

--- src/resources/assets/js/modules/enso/filters/timeFromNow.js
+++ src/resources/assets/js/modules/enso/filters/timeFromNow.js
@@ -1,4 +1,4 @@
 import Vue from 'vue';
-import { distanceInWordsToNow } from 'date-fns';
+import { distanceInWordsToNow } from 'date-fns/esm';
 
 Vue.filter('timeFromNow', value => distanceInWordsToNow(value));
