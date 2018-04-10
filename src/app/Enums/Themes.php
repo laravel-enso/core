6db93616d6a48132bcc88705b6168a33d3850849
mishu <mishu@xtelecom.ro>
2018-04-10 12:32:07 +0300

    updates theme enum

--- src/app/Enums/Themes.php
+++ src/app/Enums/Themes.php
@@ -6,5 +6,8 @@ use LaravelEnso\Helpers\app\Classes\Enum;
 
 class Themes extends Enum
 {
-    protected static $config = 'enso.themes';
+    protected static function attributes()
+    {
+        return config('enso.themes');
+    }
 }
