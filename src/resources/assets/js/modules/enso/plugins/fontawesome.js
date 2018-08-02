c9c1e13acd2496299bbe36cb43e75978c5b8ba60
mishu <mishu@xtelecom.ro>
2018-08-02 10:09:22 +0300

    fixes fontAwesomeIcon import

--- src/resources/assets/js/modules/enso/plugins/fontawesome.js
+++ src/resources/assets/js/modules/enso/plugins/fontawesome.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faListAlt } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
