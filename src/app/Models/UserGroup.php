4fe8fdbfe9f5eea1466a96c305a8578307568782
mishu <mishu@xtelecom.ro>
2018-10-11 15:39:48 +0300

    removes unused traits from UserGroup

--- src/app/Models/UserGroup.php
+++ src/app/Models/UserGroup.php
@@ -3,19 +3,13 @@
 namespace LaravelEnso\Core\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use LaravelEnso\Contacts\app\Traits\Contactable;
 use LaravelEnso\RoleManager\app\Traits\HasRoles;
-use LaravelEnso\Discussions\app\Traits\Discussable;
 use LaravelEnso\ActivityLog\app\Traits\LogsActivity;
-use LaravelEnso\CommentsManager\app\Traits\Commentable;
-use LaravelEnso\AddressesManager\app\Traits\Addressable;
-use LaravelEnso\DocumentsManager\app\Traits\Documentable;
 use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 class UserGroup extends Model
 {
-    use HasRoles, LogsActivity, Contactable, Commentable,
-        Discussable, Documentable, Addressable;
+    use HasRoles, LogsActivity;
 
     protected $fillable = ['name', 'description'];
 
