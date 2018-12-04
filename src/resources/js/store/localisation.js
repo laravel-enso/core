a8a727917b67f99d066df9266f39d5081886cd28
Jonas De Kegel <jonas@jlss.eu>
2018-12-04 16:52:02 +0100

    Edit localisation store to Support parameters in __

--- src/resources/js/store/localisation.js
+++ src/resources/js/store/localisation.js
@@ -6,11 +6,32 @@ export const state = {
 
 export const getters = {
     isInitialised: state => Object.keys(state.i18n).length > 0,
-    __: (state, getters, rootState, rootGetters) => (key) => {
-        const lang = rootGetters['preferences/lang'];
-        return state.i18n[lang]
+    __: (state, getters, rootState, rootGetters) => (key, params) => {
+        const lang = rootGetters['preferences/lang']
+            || ((rootState.preferences && rootState.preferences.global)
+                ? rootState.preferences.global.lang : null);
+
+        let translation = state.i18n[lang]
             ? state.i18n[lang][key]
             : key;
+
+        if (typeof translation === 'undefined'
+            && rootState.localisation.keyCollector) {
+                store.dispatch('localisation/addMissingKey', key);
+        }
+
+        translation = translation || key;
+        if(params) {
+            translation = translation.replace(/:(\w*)/g, function(e, key) {
+                let param = params[key.toLowerCase()];
+                if(key === key.toUpperCase()) // param is uppercased
+                    param = param.toUpperCase();
+                else if(key[0] === key[0].toUpperCase()) // first letter is uppercased
+                    param = param.charAt(0).toUpperCase() + param.slice(1);
+                return param;
+            });
+        }
+        return translation;
     },
     documentTitle: (state, getters, rootState) =>
         title => (rootState.meta.extendedDocumentTitle
