44517f5a747abdda28342f516397cabc325f9f02
mishu <mishu@xtelecom.ro>
2019-01-13 14:41:31 +0200

    small refactor

--- src/resources/js/modules/enso/filters/shortNumber.js
+++ src/resources/js/modules/enso/filters/shortNumber.js
@@ -6,9 +6,12 @@ Vue.filter('shortNumber', (number, precision = 2) => {
     if (number === 0) {
         return number;
     }
+    
     const tier = Math.floor(Math.log10(number) / 3);
 
-    if (tier === 0) return number;
+    if (tier === 0) {
+        return number;
+    }
 
     const suffix = SI[tier];
     const scale = 10 ** (tier * 3);
