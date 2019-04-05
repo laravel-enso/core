ec00729691ebe983be03ce4a9472078ee2e048cf
mishu <mishu@xtelecom.ro>
2019-04-05 12:03:19 +0300

    removes duplicate favico

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -2,8 +2,6 @@
     <head>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="icon" href="/favicon.ico"/>
         <link rel="icon" type="image/png" href="/images/favicon.png">
         <link id="theme" rel="stylesheet" type="text/css" href="/themes/light/bulma.min.css">
         <link href="{{ mix('css/enso.css') }}" rel="stylesheet" type="text/css"/>
