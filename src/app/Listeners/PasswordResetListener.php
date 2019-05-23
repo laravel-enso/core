683ddafd7db9b960d0b1153acfbfc37809e18fe3
mishu <mishu@xtelecom.ro>
2019-05-23 12:25:30 +0300

    fixes password reset listener

--- src/app/Listeners/PasswordResetListener.php
+++ src/app/Listeners/PasswordResetListener.php
@@ -9,7 +9,8 @@ class PasswordResetListener
     public function handle($event)
     {
         if ((int) config('enso.auth.password.lifetime') > 0) {
-            $event->user->update(['password_updated_at' => Carbon::now()]);
+            $event->user->password_updated_at = Carbon::now();
+            $event->user->save();
         }
     }
 }
