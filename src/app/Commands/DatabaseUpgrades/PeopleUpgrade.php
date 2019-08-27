3c6563d3253d9e212b25fd9afcbe96fe6901b088
Adrian Ocneanu <aocneanu@gmail.com>
2019-08-27 10:12:39 +0300

    Update PeopleUpgrade.php

--- src/app/Commands/DatabaseUpgrades/PeopleUpgrade.php
+++ src/app/Commands/DatabaseUpgrades/PeopleUpgrade.php
@@ -9,22 +9,20 @@ class PeopleUpgrade extends DatabaseUpgrade
 {
     protected function isMigrated()
     {
-        return Schema::hasColumn('people', 'reg_com_nr');
+        return Schema::hasColumn('people', 'bank');
     }
 
     protected function migrateTable()
     {
         Schema::table('people', function (Blueprint $table) {
-            $table->string('reg_com_nr')->nullable()->after('name');
             $table->string('bank')->nullable()->after('birthday');
-            $table->string('bank_account')->nullable()->after('birthday');
+            $table->string('bank_account')->nullable()->after('bank');
         });
     }
 
     protected function rollbackMigrateTable()
     {
         Schema::table('people', function (Blueprint $table) {
-            $table->dropColumn('reg_com_nr');
             $table->dropColumn('bank');
             $table->dropColumn('bank_account');
         });
