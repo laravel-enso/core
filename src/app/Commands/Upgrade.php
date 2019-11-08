9c190b9a0e25e869c2d87f521ccc0876711a6ec9
reza <raftx24@gmail.com>
2019-11-08 17:24:03 +0330

    add calendar upgrades

--- src/app/Commands/Upgrade.php
+++ src/app/Commands/Upgrade.php
@@ -3,20 +3,7 @@
 namespace LaravelEnso\Core\app\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Schema;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\AddingInvoiceLinePermissions;
 use LaravelEnso\Core\app\Commands\DatabaseUpgrades\CalendarUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\CompaniesIndexesUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\CompaniesUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\DataImportIndexUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\DataImportUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\FilesUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\InvoiceLineUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\PeopleUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\RenamePermissions;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\RoAddressesUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\SupplierProductPivotUpgrade;
-use LaravelEnso\Core\app\Commands\DatabaseUpgrades\VersioningUpgrade;
 
 class Upgrade extends Command
 {
@@ -31,28 +18,6 @@ class Upgrade extends Command
 
     private function upgrade()
     {
-        (new RoAddressesUpgrade())->migrate();
-        (new PeopleUpgrade())->migrate();
-        (new CompaniesUpgrade())->migrate();
-        (new DataImportIndexUpgrade())->migrate();
-        (new DataImportUpgrade())->migrate();
-        (new FilesUpgrade())->migrate();
-        (new VersioningUpgrade())->migrate();
-        (new RenamePermissions($this))->handle();
-        (new AddingCalendarPermissions())->handle();
-        (new RenameEvents())->handle();
-        (new RenameReminders())->handle();
-        (new AddingCalendarToEvents())->handle();
-        (new AddingEventPermissions())->handle();
-
-        if (Schema::hasTable('client_invoices')) {
-            (new InvoiceLineUpgrade())->migrate();
-            (new AddingInvoiceLinePermissions())->migrate();
-        }
-
-        (new SupplierProductPivotUpgrade())->handle();
-        (new CompaniesIndexesUpgrade())->handle();
-
         (new CalendarUpgrade())->handle();
     }
 }
