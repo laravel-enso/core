b18d8f665abef2a64937ceb2506179273eec522a
mishu <mishu@xtelecom.ro>
2020-01-23 00:07:54 +0200

    adds missing import

--- src/app/Http/Controllers/Spa.php
+++ src/app/Http/Controllers/Spa.php
@@ -3,6 +3,7 @@
 namespace LaravelEnso\Core\App\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\App;
 use LaravelEnso\Core\App\Http\Responses\AppState;
 
 class Spa extends Controller
