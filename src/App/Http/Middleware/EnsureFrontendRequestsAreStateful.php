5f5b9a69c3c3f7f9279030e5f3f68fbef2699733
reza <raftx24@gmail.com>
2020-06-18 15:36:47 +0430

    adds EnsureFrontendRequestsAreStateful for webview

---
+++ src/App/Http/Middleware/EnsureFrontendRequestsAreStateful.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LaravelEnso\Core\App\Http\Middleware;
+
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful as Middleware;
+
+class EnsureFrontendRequestsAreStateful extends Middleware
+{
+    public static function fromFrontend($request)
+    {
+        return ! $request->header('is-webview')
+            && parent::fromFrontend($request);
+    }
+}
