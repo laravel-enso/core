6bb55d2859e3142f0c9b558cdd68e4b373b392b4
Mihai <mihai.ocneanu@gmail.com>
2020-10-08 14:32:52 +0300

    imported seeder classes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,16 @@
 namespace LaravelEnso\Core\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use LaravelEnso\Countries\Database\Seeders\CountrySeeder;
+use LaravelEnso\Localisation\Database\Seeders\LanguageSeeder;
+use LaravelEnso\Roles\Database\Seeders\RoleSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         $this->call([
-            RoleSeeder::class, //TODO import
+            RoleSeeder::class,
             UserGroupSeeder::class,
             UserSeeder::class,
             LanguageSeeder::class,
