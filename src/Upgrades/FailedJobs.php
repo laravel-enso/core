b272de21354dd4e34cec4cd7db1835bc1893edd1
Mihai <gandesc@users.noreply.github.com>
2020-10-12 12:51:18 +0000

    Apply fixes from StyleCI

--- src/Upgrades/FailedJobs.php
+++ src/Upgrades/FailedJobs.php
@@ -4,12 +4,7 @@ namespace LaravelEnso\Core\Upgrades;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use LaravelEnso\Addresses\Models\Locality;
-use LaravelEnso\Addresses\Models\Postcode as Model;
-use LaravelEnso\Upgrade\Contracts\Applicable;
-use LaravelEnso\Upgrade\Contracts\MigratesPostDataMigration;
 use LaravelEnso\Upgrade\Contracts\MigratesTable;
-use LaravelEnso\Upgrade\Contracts\Prioritization;
 
 class FailedJobs implements MigratesTable
 {
