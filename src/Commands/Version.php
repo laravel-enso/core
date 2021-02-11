ca2c7697c015ce545fea69412798a44b6f5dfb0a
Adrian Ocneanu <aocneanu@gmail.com>
2021-02-11 11:44:13 +0200

    Update Version.php

--- src/Commands/Version.php
+++ src/Commands/Version.php
@@ -9,19 +9,15 @@ class Version extends Command
 {
     protected $signature = 'enso:version';
 
-    protected $description = 'Update version to the lastest version';
+    protected $description = 'Display framework version';
 
     public function handle()
     {
         $version = (new Service());
         $this->info("Current version is {$version->current()}");
-        $this->info("Latest version is {$version->latest()}");
-        $this->info("Core version is {$version->coreVersion()}");
 
-        if ($version->isOutdated() && $this->confirm('Your application version is outdated, Do you want to update to the core version?')) {
-            $version->update();
-
-            $this->info("Your version is {$version->coreVersion()} now");
+        if ($version->isOutdated()) {
+            $this->warn("Latest version is {$version->latest()}");
         }
     }
 }
