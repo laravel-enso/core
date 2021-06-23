9743ce591bda6d83daf31ce813eccd6179d88384
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2021-06-23 14:59:18 +0300

    extracts sentry error handling to external package

--- routes/api.php
+++ routes/api.php
@@ -2,16 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use LaravelEnso\Core\Http\Controllers\Guest;
-use LaravelEnso\Core\Http\Controllers\Sentry;
 
 Route::prefix('api')
     ->group(function () {
         Route::get('/meta', Guest::class)->name('meta');
 
         require __DIR__.'/app/auth.php';
-
-        Route::middleware(['api', 'auth'])
-            ->group(fn () => Route::get('/sentry', Sentry::class)->name('sentry'));
 
         Route::middleware(['api', 'auth', 'core'])
             ->group(fn () => require __DIR__.'/app/core.php');
