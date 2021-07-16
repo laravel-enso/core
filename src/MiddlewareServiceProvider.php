e28dc8d4bbaed5923a211a3b00d9defedbfc2da5
mishu <mishu@xtelecom.ro>
2021-07-16 10:01:31 +0300

    style update

--- src/MiddlewareServiceProvider.php
+++ src/MiddlewareServiceProvider.php
@@ -4,7 +4,7 @@ namespace LaravelEnso\Core;
 
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\ActionLogger\Http\Middleware\ActionLogger;
-use LaravelEnso\Core\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use LaravelEnso\Core\Http\Middleware\EnsureFrontendRequestsAreStateful as Stateful;
 use LaravelEnso\Core\Http\Middleware\VerifyActiveState;
 use LaravelEnso\Core\Http\Middleware\XssSanitizer;
 use LaravelEnso\Impersonate\Http\Middleware\Impersonate;
@@ -15,20 +15,14 @@ class MiddlewareServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app['router']->aliasMiddleware(
-            'verify-active-state',
-            VerifyActiveState::class
-        );
+        $this->app['router']
+            ->aliasMiddleware('verify-active-state', VerifyActiveState::class);
 
-        $this->app['router']->aliasMiddleware(
-            'xss-sanitizer',
-            XssSanitizer::class
-        );
+        $this->app['router']
+            ->aliasMiddleware('xss-sanitizer', XssSanitizer::class);
 
-        $this->app['router']->aliasMiddleware(
-            'ensure-frontent-requests-are-stateful',
-            EnsureFrontendRequestsAreStateful::class
-        );
+        $this->app['router']
+            ->aliasMiddleware('ensure-frontent-requests-are-stateful', Stateful::class);
 
         $this->app['router']->middlewareGroup('core-api', [
             VerifyActiveState::class,
