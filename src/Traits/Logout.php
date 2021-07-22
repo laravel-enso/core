80bade02d79d591f9d20ce0823a0cc0647ff3cc7
mishu <mishu@xtelecom.ro>
2021-07-22 18:39:23 +0300

    reverts logout

--- src/Traits/Logout.php
+++ src/Traits/Logout.php
@@ -4,23 +4,16 @@ namespace LaravelEnso\Core\Traits;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Sanctum\TransientToken;
 
 trait Logout
 {
     public function logout(Request $request)
     {
-        if ($this->shouldLogout($request)) {
+        if ($request->attributes->get('sanctum')) {
             Auth::guard('web')->logout();
             $request->session()->invalidate();
         } else {
             $request->user()->currentAccessToken()->delete();
         }
-    }
-
-    private function shouldLogout(Request $request): bool
-    {
-        return $request->attributes->get('sanctum')
-            || $request->user()->currentAccessToken() instanceof TransientToken;
     }
 }
