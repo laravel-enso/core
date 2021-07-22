cfb3a5056b2ec88c4f6387a73b2a37f7c35dced8
mishu <mishu@xtelecom.ro>
2021-07-22 16:43:15 +0300

    improves Logout trait to work with non spa env

--- src/Traits/Logout.php
+++ src/Traits/Logout.php
@@ -4,16 +4,23 @@ namespace LaravelEnso\Core\Traits;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Sanctum\TransientToken;
 
 trait Logout
 {
     public function logout(Request $request)
     {
-        if ($request->attributes->get('sanctum')) {
+        if ($this->shouldLogout($request)) {
             Auth::guard('web')->logout();
             $request->session()->invalidate();
         } else {
             $request->user()->currentAccessToken()->delete();
         }
+    }
+
+    private function shouldLogout(Request $request): bool
+    {
+        return $request->attributes->get('sanctum')
+            || $request->user()->currentAccessToken() instanceof TransientToken;
     }
 }
