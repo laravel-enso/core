145d36f10cb2e94938ff131d965de35cbe1faf4b
mishu <mishu@xtelecom.ro>
2023-03-23 19:53:52 +0200

    fixes email icons

--- resources/views/mail/html/footer.blade.php
+++ resources/views/mail/html/footer.blade.php
@@ -5,28 +5,32 @@
                 <td class="content-cell" align="center">
                     <p>
                     @if(config('enso.config.facebook'))
-                        <a href="{{ config('enso.config.facebook') }}">
+                        <a href="{{ config('enso.config.facebook') }}"
+                            style="text-decoration: unset">
                             <img src="{{ url('images/emails/facebook.svg') }}"
                                 alt="facebook"
                                 style="max-width: 32px">
                         </a>
                     @endif
                     @if(config('enso.config.instagram'))
-                        <a href="{{ config('enso.config.instagram') }}">
+                        <a href="{{ config('enso.config.instagram') }}"
+                            style="text-decoration: unset">
                             <img src="{{ url('images/emails/instagram.svg') }}"
                                 alt="instagram"
                                 style="max-width: 32px">
                         </a>
                     @endif
                     @if(config('enso.config.twitter'))
-                        <a href="{{ config('enso.config.twitter') }}">
+                        <a href="{{ config('enso.config.twitter') }}"
+                            style="text-decoration: unset">
                             <img src="{{ url('images/emails/twitter.svg') }}"
                                 alt="twitter"
                                 style="max-width: 32px">
                         </a>
                     @endif
                     @if(config('enso.config.tiktok'))
-                        <a href="{{ config('enso.config.tiktok') }}">
+                        <a href="{{ config('enso.config.tiktok') }}"
+                            style="text-decoration: unset">
                             <img src="{{ url('images/emails/tiktok.svg') }}"
                                 alt="tiktok"
                                 style="max-width: 32px">
