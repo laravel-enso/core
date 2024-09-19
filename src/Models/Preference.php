fd70f664a9b74abfd29b36437e197d2fececcfae
Abdullahi Abdul Kabir <abdullahiabdulkabir1@gmail.com>
2024-09-19 12:45:54 +0100

    Laravel 11

--- src/Models/Preference.php
+++ src/Models/Preference.php
@@ -14,10 +14,15 @@ class Preference extends Model
 
     protected $guarded = ['id'];
 
-    protected $casts = ['value' => 'object'];
-
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'value' => 'object',
+        ];
     }
 }
