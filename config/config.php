1c870c27215be0c2dc05535ee28eab02d5930ccc
mishu <mishu@xtelecom.ro>
2025-05-22 13:19:07 +0300

    adds youtube to config

--- config/config.php
+++ config/config.php
@@ -1,15 +1,16 @@
 <?php
 
 return [
-    'version'               => '4.8.0',
-    'ownerCompanyId'        => env('OWNER_COMPANY_ID', 1),
-    'showQuote'             => env('SHOW_QUOTE', true),
-    'defaultRole'           => 'admin',
-    'dateFormat'            => 'd-m-Y',
-    'dateTimeFormat'        => 'd-m-Y H:i:s',
-    'facebook'              => 'https://facebook.com',
-    'instagram'             => 'https://www.instagram.com',
-    'twitter'               => 'https://twitter.com',
-    'tiktok'                => 'https://tiktok.com',
+    'version' => '4.8.0',
+    'ownerCompanyId' => env('OWNER_COMPANY_ID', 1),
+    'showQuote' => env('SHOW_QUOTE', true),
+    'defaultRole' => 'admin',
+    'dateFormat' => 'd-m-Y',
+    'dateTimeFormat' => 'd-m-Y H:i:s',
+    'facebook' => 'https://facebook.com',
+    'instagram' => 'https://www.instagram.com',
+    'twitter' => 'https://twitter.com',
+    'tiktok' => 'https://tiktok.com',
+    'youtube' => 'https://youtube.com',
     'extendedDocumentTitle' => env('EXTENDED_DOCUMENT_TITLE', true),
 ];
