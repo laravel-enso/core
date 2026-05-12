c6feba5f4e80b0a16c28a26ceeede9a6ba57cdd5
Adrian Ocneanu <aocneanu@gmail.com>
2026-05-12 15:08:58 +0300

    Add logins created at index to migration

--- database/migrations/2017_01_01_114000_create_logins_table.php
+++ database/migrations/2017_01_01_114000_create_logins_table.php
@@ -17,6 +17,8 @@ return new class() extends Migration {
             $table->text('user_agent');
 
             $table->timestamps();
+
+            $table->index('created_at');
         });
     }
 
