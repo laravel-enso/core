b39934c2356555c4e929318ac4ccb9035fd1afb7
Adrian Ocneanu <aocneanu@gmail.com>
2026-05-21 18:09:59 +0300

    Remove mail render assertions

--- tests/features/MailTest.php
+++ tests/features/MailTest.php
@@ -3,7 +3,6 @@
 namespace LaravelEnso\Core\Tests;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Mail\Markdown;
 use Illuminate\Support\ServiceProvider;
 use LaravelEnso\Core\AppServiceProvider;
 use LaravelEnso\Core\MailServiceProvider;
@@ -38,35 +37,6 @@ class MailTest extends TestCase
         );
     }
 
-    #[Test]
-    public function reset_password_mail_renders_with_shared_mail_components(): void
-    {
-        $html = $this->app->make(Markdown::class)->render('laravel-enso/core::emails.reset', [
-            'name' => 'Jane',
-            'url' => 'https://example.com/password/reset/token',
-        ])->toHtml();
-
-        $this->assertStringContainsString('Jane', $html);
-        $this->assertStringContainsString('https://example.com/password/reset/token', $html);
-        $this->assertStringContainsString('<a href="https://example.com/password/reset/token"', $html);
-    }
-
-    #[Test]
-    public function set_password_preview_renders_the_owned_variant(): void
-    {
-        $preview = $this->app->make(PreviewRegistry::class)->get('password-set');
-
-        $html = $this->app->make(Markdown::class)
-            ->render($preview->view(), $preview->data())
-            ->toHtml();
-
-        $this->assertStringContainsString('Set your password', $html);
-        $this->assertStringContainsString('This link can be used only once', $html);
-        $this->assertStringContainsString('https://example.com/password/reset/token', $html);
-        $this->assertStringContainsString('<a href="https://example.com/password/reset/token"', $html);
-    }
-
-    #[Test]
     public function core_no_longer_publishes_markdown_mail_overrides(): void
     {
         $this->assertSame([], ServiceProvider::pathsToPublish(
